c5a143c427b34e75cf79fd5faaaf66fbbc62e9a9
ijse <i@ijser.cn>
2015-04-02 17:16:22 +0800

    07 done

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -4,5 +4,13 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* () {
-  
+    if(this.request.get('Content-Type') === 'text/plain') {
+        this.body = 'ok';
+    } else {
+        this.response.set('Content-Type', 'application/json');
+        this.body = {
+            message: 'hi!'
+        };
+    }
+
 })
